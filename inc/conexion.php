ac33d9f36d03adba1a62a25b1c540d370f098cbf
brayanAchata <brayan.achata@gmail.com>
2016-12-15 19:35:06 -0500

    olaa

--- inc/conexion.php
+++ inc/conexion.php
@@ -3,7 +3,7 @@
 	</head>
 	<body>
 		<?
-			$conexion = new mysqli("mysql.hostinger.es","u528603932_u2226","root",'u528603932_u2226');
+			$conexion = new mysqli("mysql.hostinger.es","u528603932_braya","brayan01",'u528603932_braya');
 			if($conexion->conect_errno){
 				echo "Error al connectar a la BBDD".
 					$conexion->connect_errno.",".
